407d8b489f5a9f7b7510ebe4e713229f8b503129
Antonio Adami <antonioadamipalma@gmail.com>
2021-03-03 23:29:36 -0300

    Não gerar pedido se quantiadde é maior do que tem. Atualizar quantidade

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
 
@@ -43,18 +44,45 @@ class CreateOrderService {
       throw new AppError('Ha um produto invalido');
     }
 
-    const orderProducts = products.map(product => ({
-      product_id: product.id,
-      price:
-        (productsExists.find(find => find.id === product.id)?.price || 0) *
-        product.quantity,
-      quantity: product.quantity,
-    }));
+    const orderProducts = products.map(product => {
+      const existingProduct = productsExists.find(
+        find => find.id === product.id,
+      );
+      if (!existingProduct) {
+        throw new AppError('Nao encontrou igual');
+      }
+      if (existingProduct.quantity < product.quantity) {
+        throw new AppError('Nao pode comprar mais do que tem');
+      }
+      return {
+        product_id: product.id,
+        price: existingProduct.price * product.quantity,
+        quantity: product.quantity,
+      };
+    });
 
     const order = await this.ordersRepository.create({
       customer,
       products: orderProducts,
     });
+
+    if (!order) {
+      throw new AppError('Order nao foi criada');
+    }
+
+    const updateQuantity: IUpdateProductsQuantityDTO[] = productsExists.map(
+      product => {
+        const subtractQuant =
+          products.find(find => find.id === product.id)?.quantity || 0;
+
+        return {
+          id: product.id,
+          quantity: product.quantity - subtractQuant,
+        };
+      },
+    );
+
+    await this.productsRepository.updateQuantity(updateQuantity);
 
     return order;
   }
