73e5a948f5baee502364a1dca36369f57a7a9c5f
Antonio Adami <antonioadamipalma@gmail.com>
2021-03-03 21:51:42 -0300

    Correção serviço createCostumer

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -20,7 +20,7 @@ class CreateCustomerService {
   public async execute({ name, email }: IRequest): Promise<Customer> {
     const customerExists = await this.customersRepository.findByEmail(email);
 
-    if (!customerExists) {
+    if (customerExists) {
       throw new AppError('Email ja cadastrado');
     }
 
